d5cefa5ceebcdf4dc0314dc45476754c0530166c
Md Tuser Sheikh <tuser.sheikh@zoho.com>
2022-02-05 16:30:12 +0600

    serve image as static folder

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import booksRouter from './routes/books/books.router.js';
 const app = express();
 
 app.use(express.json());
+
+app.use('/images', express.static('images'));
 app.use('/books', booksRouter);
 
 export default app;
