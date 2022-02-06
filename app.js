3c1a27676a383e9495c7ae1735f289a460afa4d0
Md Tuser Sheikh <tuser.sheikh@zoho.com>
2022-02-06 12:33:20 +0600

    add morgan for logs

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import morgan from 'morgan';
+import { createWriteStream } from 'fs';
 
 import booksRouter from './routes/books/books.router.js';
 
 const app = express();
+const logStream = createWriteStream('logs/access.log', { flags: 'a' });
 
 app.use(express.json());
+
+app.use(morgan('combined', { stream: logStream }));
+app.use(morgan('combined'));
 
 app.use('/images', express.static('images'));
 app.use('/books', booksRouter);
